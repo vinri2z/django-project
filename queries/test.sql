fcccd484c31a24dd3d0dcc6432b920dfce151b1d
Vincent Rizzo <vincent@datamaran.com>
2020-12-15 16:51:33 -0600

    First tests

---
+++ queries/test.sql
@@ -0,0 +1,13 @@
+-- DROP TABLE test;
+
+-- CREATE TABLE test (
+-- 	id int,
+-- 	first varchar(255),
+-- 	last varchar(255),
+-- 	age int
+-- );
+
+INSERT INTO test (id, first, last, age)
+VALUES (2,'vincent', 'rizzo', 25);
+
+SELECT * FROM test;
